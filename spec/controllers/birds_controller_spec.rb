d3818adce61f8ea60c8468f351394910b85a3de5
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2016-05-20 23:28:06 +0200

    Test the one action in birds controller as well

--- spec/controllers/birds_controller_spec.rb
+++ spec/controllers/birds_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe BirdsController, type: :controller do
-
+  describe "show" do
+    context "bird" do
+      it "should return json-api with birdcat relation and sideload" do
+        bird = create(:named_bird)
+        get :show, id: bird.id
+        expect(json['data']).to_not be(nil)
+        expect(json['data']['attributes']['name']).to_not be(nil)
+        expect(json['data']['attributes']['name']['en']).to eq(bird.name(lang: 'en'))
+        expect(json['included'].first['attributes']['name']['en']).to eq(bird.birdcat.name(lang: 'en'))
+      end
+    end
+  end
 end
